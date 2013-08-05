bef920f7b9b90a21a5fdf01a2a5501a9703d2d0c
Roman Manasyan <rmanasyan@gmail.com>
2013-08-06 00:15:08 +0300

    share buttons from pluso.ru

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+/**
+ * Main
+ */
 (function ($) {
 
 })(jQuery);
+
+/**
+ * Share.pluso.ru
+ */
+//(function() {
+//    if (window.pluso)if (typeof window.pluso.start == "function") return;
+//    var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
+//    s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
+//    s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
+//    var h=d[g]('head')[0] || d[g]('body')[0];
+//    h.appendChild(s);
+//})();
