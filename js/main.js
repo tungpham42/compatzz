6de57c568df844ba150cdab44e24db86288ef90e
Roman Manasyan <rmanasyan@gmail.com>
2013-08-06 13:24:19 +0300

    prefill date dropdowns, percent bars resizing

--- js/main.js
+++ js/main.js
@@ -2,6 +2,51 @@
  * Main
  */
 (function ($) {
+    'use strict';
+
+    var Main = {
+        init: function() {
+            this.appendDateOptions();
+            this.resizeBars();
+        },
+
+        appendDateOptions: function() {
+            var days = [], months = [], years = [];
+
+            // days/months range
+            for (var i = 1; i <= 31; i++) {
+                var num = ('0' + i).slice(-2);
+                days.push('<option value="' + num + '">' + num + '</option>');
+
+                // push months as well
+                if (i <= 12) {
+                    months.push('<option value="' + num + '">' + num + '</option>');
+                }
+            }
+
+            // years range
+            for (var y = 2000; y >= 1950; y--) {
+                years.push('<option value="' + y + '">' + y + '</option>');
+            }
+
+            $('select[data-option=dd]').append(days.join(''));
+            $('select[data-option=mm]').append(months.join(''));
+            $('select[data-option=yyyy]').append(years.join(''));
+        },
+
+        resizeBars: function() {
+            var cycles = { physical: '81%', emotional: '92%', intellectual: '66%', total: '90%' };
+            $.each(cycles, function(i, val) {
+                var $context = $('.' + i);
+                $context.attr('title', $context.attr('title') + ': ' + val);
+                $('.percent', $context).html(val);
+                $('.percentage-bar', $context).width(val);
+            });
+
+        }
+    };
+
+    Main.init();
 
 })(jQuery);
 
