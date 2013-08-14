5d3336dcb9fefaeb239b2daed1cef18089c6d782
Roman Manasyan <rmanasyan@gmail.com>
2013-08-14 20:46:21 +0300

    removed share buttons code

--- js/main.js
+++ js/main.js
@@ -184,15 +184,3 @@
     Main.init();
 
 })(jQuery);
-
-/**
- * Share.pluso.ru
- */
-//(function() {
-//    if (window.pluso)if (typeof window.pluso.start == "function") return;
-//    var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
-//    s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
-//    s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
-//    var h=d[g]('head')[0] || d[g]('body')[0];
-//    h.appendChild(s);
-//})();
